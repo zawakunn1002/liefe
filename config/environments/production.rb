3f6d7106f8e57ecda6c64f6922d4800eea70ace9
zawakunn1002 <bumpk1002@gmail.com>
2019-07-27 10:43:42 +0000

    [update-password]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,11 +103,11 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
-  port:                  587,
+  port:                  465,
   domain:               'gmail.com',
   user_name:            mail,
   password:             pass,
   authentication:       'plain',
-  enable_starttls_auto:  true
+  ssl:                   true
 }
 end
